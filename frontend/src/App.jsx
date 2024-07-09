12ecf8045d01b1e41dd1e0053d69dda8a14d5393
Dipro <dipropaul032@gamil.com>
2024-07-09 12:53:25 +0600

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,12 @@ function App() {
       <button className="btn btn-accent">Accent</button>
       <button className="btn btn-ghost">Ghost</button>
       <button className="btn btn-link">Link</button>
+      <div>
+      <button className="btn btn-outline btn-info">Info</button>
+<button className="btn btn-outline btn-success">Success</button>
+<button className="btn btn-outline btn-warning">Warning</button>
+<button className="btn btn-outline btn-error">Error</button>
+      </div>
     </>
   )
 }
