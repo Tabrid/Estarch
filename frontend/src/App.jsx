4bb2e5b235e9a2adf91ee71192ea2d24d352075e
Dipro <dipropaul032@gamil.com>
2024-07-09 19:59:11 +0600

    addefss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,27 +3,7 @@ function App() {
 
   return (
     <>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-      <div>
-      <button className="btn btn-outline btn-info">Info</button>
-<button className="btn btn-outline btn-success">Success</button>
-<button className="btn btn-outline btn-warning">Warning</button>
-        <button className="btn btn-outline btn-error">Error</button>
-        
-
-        <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Responsive</button>
-
-        <button className="btn btn-wide">Wide</button>
-        <button className="btn btn-wide">Wide</button>
-        <button className="btn btn-wide">Wide</button>
-        <button className="btn btn-wide">Wide</button>
-      </div>
+    
     </>
   )
 }
