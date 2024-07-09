24c11bf69d51b98d1e0747deaab9ea02df34f7ae
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-07-09 22:45:37 +0600

    add: header banner

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,6 +1,9 @@
+import HeaderBanner from "../../Components/HeaderBanner/HeaderBanner"
+
 function Home() {
   return (
     <div>
+      <HeaderBanner></HeaderBanner>
       <h1>home page</h1>
     </div>
   )
